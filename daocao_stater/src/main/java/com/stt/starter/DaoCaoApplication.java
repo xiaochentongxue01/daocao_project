de87d0bf1dbdf1d83940ddb5ef0cd66d2e71a585
陈开建 <14799698+chen-kaijian123@user.noreply.gitee.com>
2024-11-01 21:33:39 +0800

    初始化项目

---
+++ daocao_stater/src/main/java/com/stt/starter/DaoCaoApplication.java
@@ -0,0 +1,11 @@
+package com.stt.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoCaoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DaoCaoApplication.class, args);
+    }
+}
